f4a8aa9a52e7ad74d9da7b184b0c36d8a65898c5
Francis Kodama <fk@fkodama.com>
2026-01-05 13:45:52 -0500

    Prisma end

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,26 @@
-import { PrismaClient } from '@prisma/client'
+import { Pool, neonConfig } from '@neondatabase/serverless';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
+import ws from 'ws';
+
+if (process.env.NODE_ENV === 'development') {
+  neonConfig.webSocketConstructor = ws;
+}
+
+const connectionString = `${process.env.DATABASE_URL}`;
 
 const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+};
 
 declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-const prisma = globalThis.prisma ?? prismaClientSingleton()
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-export default prisma
+export default prisma;
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
